7c1f47323a75112aa2c61047a3d5a94f46541014
Paul Farrell <87060717+paulbfarrell007@users.noreply.github.com>
2022-01-25 21:24:31 -0500

    Add files via upload

--- week4/assignments/langs/languages2.js
+++ week4/assignments/langs/languages2.js
@@ -3,17 +3,30 @@ const lang = 'JavaScript'
 // Create an event listener for the submit button that adds all 'input' elements to 
 // a single array using the spread operator. Call the chkLang() function, passing in 
 // the array as an argument.
+document.getElementById('submit').addEventListener('click', function() {
+    const langs = document.querySelectorAll("input")
+    const langArray = [...langs]
+    chkLang(langArray)
+    
+
+})
 
 
-function chkLang(langs) {
+function chkLang(langArray) {
     let result = false
 
     // use an array method to check whether the user included 'JavaScript' in their
     // list of languages
+    langArray.forEach(item => {
+        if (item.value === lang)
+        result = true        
+    });
+
 
     let obj = document.querySelector('#TestResult')
     if (result) 
         obj.innerText = `Congratulations!\nYou know ${lang}.`
     else
         obj.innerText = `Sorry,\nyou don't know ${lang}.`
+
 }
