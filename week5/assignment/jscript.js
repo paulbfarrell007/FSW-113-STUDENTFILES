225608a592d5627f2de3acb57528f03a247b72c5
Paul Farrell <87060717+paulbfarrell007@users.noreply.github.com>
2022-02-06 12:55:16 -0500

    Add files via upload

--- week5/assignment/jscript.js
+++ week5/assignment/jscript.js
@@ -1,14 +1,63 @@
 // Declare any necessary variables.
 
+var eName = document.getElementById("studentName");
+var eClass = document.getElementById("className");
+var eCurrent = document.getElementById("studentScores");
+var ePossible = document.getElementById("possibleScores");
+var eReset = document.getElementById("reset");
+var ePrint = document.getElementById("print");
+var eStudentName = document.getElementById("certStudentName");
+var eClassName = document.getElementById("certClassName");
+var eGradeName = document.getElementById("certGrade");
+var oStudent = {name: eName.value, class: eClass.value, grade: ''};
+
 // Add am evemt listener that responds to the click of the "print" button by calling a function to instantiate
 //  a new student object, and another function to print the certificate.
+
+var ePrint = document.getElementById("print");
+ePrint.addEventListener("click", Grade);
 
 // Add an event listener that responds to the click of the reset button by resetting all the values
 // both in the form and in the certificate.
 
+function resetPage(){
+    eName.value = ' ';
+    eClass.value = ' ';
+    eCurrent.value = ' ';
+    ePossible.value = ' ';
+
+    eName.textContent = ' ';
+    eClass.textContent = ' ';
+    eCurrent.textContent = ' ';
+    ePossible.textContent = ' ';
+console.log('Rest Clicked');
+}
+
+eReset.addEventListener("click",  resetPage);
+
 // Create a function that instantiates a new student object with the input from the HTML form.
+
+// ??????????????????????????????????
 
 // Create a function that fills in the student's name, class name, and calculated grade on the certificate .
 
+function Grade(){
+    
+    console.log(`Name: ${oStudent.name}, Class of: ${oStudent.class}, GPA: ${oStudent.grade}`)
+    eStudentName.textContent = oStudent.name
+    eClassName.textContent = oStudent.class
+    eGradeName.textContent = oStudent.grade
+}
+
 // Create a function that converts the contents of a comma-separated text string to a numeric array.
 // You can use this function when instantiating the arrays in the student object.
+
+function GetGradeArray(element){ // Quick array maker, this was taken from, Week 3: gradeCurver
+    eArray = element.value.split(','); // Make each it's own section
+    eArray = eArray.map(function (Mapped) { // Map it out
+        return parseInt(Mapped); // Return Map
+    })
+    return eArray; // Return Array
+}
+/*
+*/
--- week5/assignment/student.js
+++ week5/assignment/student.js
@@ -3,3 +3,66 @@
 // - a method that adds up all the student's scores
 // - a method that adds up all the possible scores
 // - a method that calculates the student's letter grade (divide the student's score by the possible scores and use the resulting decimal to determine grade)
+//
+
+var ePrint = document.getElementById("print");
+var eName = document.getElementById("studentName");
+var eClass = document.getElementById("className");
+var eCurrent = document.getElementById("studentScores");
+var ePossible = document.getElementById("possibleScores");
+
+eCurrent = GetGradeArray(eCurrent);
+ePossible = GetGradeArray(ePossible);
+var oStudent;
+
+function GetGradeArray(element){ // Quick array maker, this was taken from, Week 3: gradeCurver
+    eArray = element.value.split(','); // Make each it's own section
+    eArray = eArray.map(function (Mapped) { // Map it out
+        return parseInt(Mapped); // Return Map
+    })
+    return eArray; // Return Array
+}
+function GetAverageGrade(array){
+    var iValue = 0; // start with 0
+    for (i=0; i<array.length; i++) // loop all numbers in array
+    {iValue += parseInt(array[i]);}// add all the numbers in the array
+    iValue /= array.length; // divide all the added up numbers
+    iValue = parseInt(iValue); // make sure it's an intiger
+    return iValue;
+}
+function ConvertScores(float){
+    var sGrade = ''; // String grade, see below
+    if(float < 0.66)                {sGrade = '0.0  F'}
+    if(float < 0.67 && float > 0.64){sGrade = '1.0  D'}
+    if(float < 0.68 && float > 0.66){sGrade = '1.3 +D'}
+    if(float < 0.73 && float > 0.71){sGrade = '1.7 -C'}
+    if(float < 0.77 && float > 0.72){sGrade = '2.0  C'}
+    if(float < 0.80 && float > 0.76){sGrade = '2.3 +C'}
+    if(float < 0.83 && float > 0.79){sGrade = '2.7 -B'}
+    if(float < 0.87 && float > 0.82){sGrade = '3.0  B'}
+    if(float < 0.90 && float > 0.86){sGrade = '3.3 +B'}
+    if(float < 0.93 && float > 0.89){sGrade = '3.7 -A'}
+    if(float < 0.97 && float > 0.92){sGrade = '4.0  A'}
+    if(float > 0.96)                {sGrade = '4.0 +A'}
+    return sGrade; // Return the grade
+}
+function GradeScores(Grades, Scores){
+    var current = GetAverageGrade(Grades); // average scores
+    var max = GetAverageGrade(Scores); // average max scores
+// (divide the student's score by the possible scores and use the resulting decimal to determine grade)
+    var divide = current / max; // get the float of the grade
+    var iScore = ConvertScores(divide); // get the GPS
+    return iScore;
+}
+
+function Grade(){
+    
+    var GPA = GradeScores(eCurrent, ePossible);
+    oStudent = {name: eName.value, class: eClass.value, grade: GPA};
+    console.log(`Name: ${oStudent.name}, Class of: ${oStudent.class}, GPA: ${oStudent.grade}`)
+}
+
+var ePrint = document.getElementById("print");
+ePrint.addEventListener("click", Grade);
+/*
+*/
